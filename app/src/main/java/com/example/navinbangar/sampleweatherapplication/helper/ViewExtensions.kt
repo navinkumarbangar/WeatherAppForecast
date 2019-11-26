45d6e6f380526613491d93520cd74d75b594beab
navinkumarbangar <naveen.bangar27@gmail.com>
2019-11-26 14:38:22 +0530

    updated ui implementation- implemented view extensions functions

---
+++ app/src/main/java/com/example/navinbangar/sampleweatherapplication/helper/ViewExtensions.kt
@@ -0,0 +1,29 @@
+package com.example.navinbangar.sampleweatherapplication.helper
+
+import android.content.Context
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
+
+/**
+ * Created by Navin Bangar on 11/26/2019.
+ */
+
+fun View.makeVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.makeGone() {
+    visibility = View.GONE
+}
+
+fun View.hideKeyboard(context: Context) {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun View.viewVisibleAnimator() {
+    val inAnimation = AnimationUtils.loadAnimation(this.context, android.R.anim.fade_in)
+    this.startAnimation(inAnimation)
+    this.makeVisible()
+}
